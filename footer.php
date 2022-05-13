b7872bdd22d9291d6fe2446fae661174ad2131fb
fede.toldo96@gmail.com <fede.toldo96@gmail.com>
2022-05-13 12:54:53 +0200

    Stilizzazione efixing problemi home page

--- footer.php
+++ footer.php
@@ -1,42 +1,53 @@
+<footer>
+
 <section class="container-fluid">
-    <div class="row">
-        <img src="" alt="">
-    </div>
-
-    <div class="container">
-        <div class="cta row justify-content-between">
-            <h2> Vuoi sapere di più? Scrivici </h2>
-
-            <a href="">
-                <button> Contatti </button>
-            </a>
+    <div class="cover">
+        <div class="row">
+            <img src="" alt="">
         </div>
     </div>
+
+        <div class="container">
+            <div class="cta row justify-content-between">
+                <h2> Vuoi sapere di più? Scrivici </h2>
+
+                <a href="">
+                    <button> Contatti </button>
+                </a>
+            </div>
+        </div>
+
 </section>
 
-<footer class="container-fluid">
+<div class="contact container-fluid">
     <div class="container">
-        <div class="row">
+        <div class="contenuti row justify-content-between">
             <div class="col-2">
-                logo
+                <a href="">
+                    <img src="<?php echo get_template_directory_uri(); ?>\img\logo\logo-footer.png" alt="">
+                </a>
             </div>
 
             <div class="social"></div>
 
             <div class="contatti">
-                <a href="tel:3473411518" target="_blank"> Matteo +39 347 3411518</a>
-                <a href="mailto:wildtrackvalsugana@gmail.com" target="_blank"> wildtrackvalsugana@gmail.com </a>
+                <a href="tel:3473411518" target="_blank"> <p>Matteo +39 347 3411518</p></a>
+                <br>
+                <a href="mailto:wildtrackvalsugana@gmail.com" target="_blank"> <p>wildtrackvalsugana@gmail.com</p></a>
             </div>
 
             <div class="link">
-                <a href=""> Privacy & Cookies </a>
-                <a href="" target="_blank"> 2022 credits </a>
+                <a href=""> <h6> Privacy & Cookies </h6>  </a>
+                <br>
+                <a href="" target="_blank"> <h6> 2022 credits </h6> </a>
             </div>
         </div>
     </div>
-</footer>
+</div>
 
 <?php wp_footer(); ?>
+
+</footer>
 
 </main>
 
